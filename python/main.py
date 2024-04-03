79549963305a71c1d949b3aa2d7e609bf975a54d
Robert <rrochlin@yahoo.com>
2024-04-03 00:05:30 -0700

    fixing logging

--- python/main.py
+++ python/main.py
@@ -11,20 +11,21 @@ intents = discord.Intents(
 )
 bot = commands.Bot(command_prefix="-", intents=intents)
 
+root = logging.getLogger(__name__)
+root.propagate = True
+root.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+
 
 async def main():
     await bot.add_cog(music_cog(bot))
 
 
 if __name__ == "__main__":
-    root = logging.getLogger(__name__)
-    root.propagate = True
-    root.setLevel(logging.DEBUG)
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    root.addHandler(handler)
 
     asyncio.run(main())
     config = ConfigParser()
