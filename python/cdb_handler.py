537d7c84a55e1ced6516cfd1087e70720e51a271
Robert <rrochlin@yahoo.com>
2024-04-04 11:02:06 -0700

    fixing logging

--- python/cdb_handler.py
+++ python/cdb_handler.py
@@ -33,8 +33,8 @@ class cdb_handler:
         self.cursor = self.engine.raw_connection().cursor()
 
         root = logging.getLogger(__name__)
+        root.setLevel(logging.DEBUG)
         handler = logging.StreamHandler(sys.stdout)
-        handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
         root.addHandler(handler)
--- python/music_cog.py
+++ python/music_cog.py
@@ -20,8 +20,8 @@ class music_cog(commands.Cog):
         }
         self.vc: discord.guild.VocalGuildChannel = None
         root = logging.getLogger(__name__)
+        root.setLevel(logging.DEBUG)
         handler = logging.StreamHandler(sys.stdout)
-        handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
         root.addHandler(handler)
