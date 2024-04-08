e77760a269015d644a3a5961321594b2fe08022f
Robert <rrochlin@yahoo.com>
2024-04-07 22:46:16 -0700

    litestar is working but killing the process seems to not always work

--- python/app.py
+++ python/app.py
@@ -1,3 +1,4 @@
+from litestar import Litestar, get
 import discord
 from configparser import ConfigParser
 from discord.ext import commands
@@ -5,6 +6,7 @@ from music_cog import music_cog
 import asyncio
 import sys
 import logging
+from threading import Thread
 
 intents = discord.Intents(
     messages=True, guilds=True, message_content=True, voice_states=True
@@ -23,11 +25,27 @@ root.addHandler(handler)
 async def main():
     await bot.add_cog(music_cog(bot))
 
+asyncio.run(main())
+config = ConfigParser()
+config.read("Web.config")
+TOKEN = config["SECRETS"]["TOKEN"]
+thread = Thread(target=bot.run, args=(TOKEN,))
+thread.start()
 
-if __name__ == "__main__":
-    asyncio.run(main())
-    config = ConfigParser()
-    config.read("Web.config")
-    TOKEN = config["SECRETS"]["TOKEN"]
-    print(bot.status)
-    bot.run(TOKEN)
+
+@get("/")
+async def hello_world() -> str:
+    return str(bot.status)
+
+
+@get("/skip")
+async def skip() -> str:
+    root.info("running skip function")
+    try:
+        cog: music_cog = bot.get_cog("music_cog")
+        cog.vc.stop()
+        return str(cog.cdb.current_song)
+    except Exception as e:
+        root.error(e)
+
+app = Litestar([hello_world, skip])
